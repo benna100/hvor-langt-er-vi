25d1e2a42ad2b106785841bfe094b4cade07b508
Benjamin Hughes <benjamin.dals.hughes@gmail.com>
2021-02-03 21:34:10 +0100

    stuu

---
+++ cron-test/cron-test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "started"
+echo "" > /home/pi/projects/hvor-langt-er-vi/cron-test/works.txt
+echo "finished"
--- data-deploy.sh
+++ data-deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+echo "started 1"
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_ed25519
+ssh-add -l
 npm run data-deploy
+echo "finisehd"
