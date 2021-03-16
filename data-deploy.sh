50860dd466934db20a932dada8ca41644e4643d8
Benjamin Hughes <benjamin.dals.hughes@gmail.com>
2021-03-16 19:31:02 +0100

    added newest data

--- data-deploy.sh
+++ data-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "started 1"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use 14.15.5
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_ed25519
